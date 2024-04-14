308cf547a2330619ec5eceb4e582bfe881187437
Daniel Gehlen <142283217+Daniel-Gehlen@users.noreply.github.com>
2024-04-14 09:01:37 -0300

    Update menu.rb

---
+++ display/menu.rb
@@ -0,0 +1,10 @@
+# Funções para exibir o menu
+
+def display_main_menu
+  puts "\n--- Menu Principal ---"
+  puts "1. Adicionar Produto"
+  puts "2. Listar Produtos"
+  puts "3. Remover Estoque"
+  puts "4. Sair"
+  print "Digite sua escolha: "
+end

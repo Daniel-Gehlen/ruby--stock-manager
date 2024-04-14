43d8b32d477a6efe6913bcd1b7cc82b234402ca1
Daniel Gehlen <142283217+Daniel-Gehlen@users.noreply.github.com>
2024-04-14 09:00:42 -0300

    Update list_products.rb

---
+++ core/list_products.rb
@@ -0,0 +1,7 @@
+# Funções para listar produtos
+
+def display_product_list
+  # Obtem a lista de produtos (implementação específica)
+  # Exibe a lista de produtos na tela
+  # (implementação específica para formatação)
+end
